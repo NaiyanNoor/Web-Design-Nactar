e9a19d04f3bb6f2595fa1e32575d19f173c375e8
Naiyan Noor <47422181+NaiyanNoor@users.noreply.github.com>
2024-09-17 15:16:37 +0600

    Add files via upload

---
+++ JavaScript/Day 2/main1.js
@@ -0,0 +1,14 @@
+var a;
+a=500;
+var b=300;
+var sum=a+b;
+//var x=a++ + b++;
+var y=++a + ++b;
+var p=2**3
+var s=12/5;
+var d=12%5;
+
+document.write("b "+b+" a "+a+" x "+y+"<br>");
+document.write("p "+p+"<br>");
+document.write("s "+s+"<br>");
+document.write("d "+d+"<br>");
